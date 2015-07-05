6bdf5787e11b2a1313c6a922e36804c3dd93f864
Al Nguyen <al.nguyen@gmail.com>
2015-07-05 19:00:55 -0400

    Use MOBservable and create our first input field

--- app/scripts/src/react-example.jsx
+++ app/scripts/src/react-example.jsx
@@ -1,20 +1,34 @@
 // vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+/* jshint quotmark: false */
 
 'use strict';
 
 function render () {
   var React = require('react');
+  var mobservable = require('mobservable');
 
-  //var HelloMessage = React.createClass({
-  //  render: function() {
-  //    return <div>Hello {this.props.name}</div>;
-  //  }
-  //});
+  function InputData(input) {
+    mobservable.props(this, {
+      input: input
+    });
+  }
+
+  var InputView = React.createClass({
+    mixins: [mobservable.ObserverMixin],
+
+    render: function() {
+      return <input type="text" onChange={this.update} value={this.props.input.input} />;
+    },
+
+    update: function() {
+      // How do we make the input field changeable?
+      console.log(this.props.input.input);
+    }
+  });
 
   React.render(
-    //<helloMessge name="Al" />,
-    <p>Lorem Ipsum</p>,
-    document.getElementById('react-example')
+    <InputView input={new InputData('testal')} />,
+    document.getElementById('nato-input-container')
   );
 }
 
