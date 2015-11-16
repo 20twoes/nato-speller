700788f804a7de1bdd1fa74cb31c5638c483e82d
Al Nguyen <al.nguyen@gmail.com>
2015-11-15 22:54:36 -0500

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gulp
+rm -rf ~/tmp/nato-speller
+git clone git@github-20twoes:20twoes/nato-speller.git -b gh-pages --single-branch ~/tmp/nato-speller
+cp -a dist/ ~/tmp/nato-speller/
+cd ~/tmp/nato-speller
+git config user.email al.nguyen@gmail.com
+git add . -A
+git commit
+git push origin gh-pages
